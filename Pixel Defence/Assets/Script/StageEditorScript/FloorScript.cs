043ace8dce6d1ceeb8031f4c82d9cab9ae53d346
GUK <gukhyeon10@daum.net>
2019-09-24 01:58:46 +0900

    StageTool 1.1.1
    Floor 로드시 트랙 넘버가 0으로 초기화되던 로직 수정

--- Pixel Defence/Assets/Script/StageEditorScript/FloorScript.cs
+++ Pixel Defence/Assets/Script/StageEditorScript/FloorScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FloorScript : MonoBehaviour
 {
     int floorKind;
-    public int trackNumber;
+    public int trackNumber = 0;
     public int floorNumber;
 
     void Start()
@@ -13,19 +13,19 @@ public class FloorScript : MonoBehaviour
         if(this.name.Equals("StartFloor"))
         {
             floorKind = (int)FloorKind.START_FLOOR;
-            trackNumber = 0;
+            //trackNumber = 0;
             floorNumber = -1;
         }
         else if(this.name.Equals("MiddleFloor"))
         {
             floorKind = (int)FloorKind.MIDDLE_FLOOR;
-            trackNumber = 0;
+            //trackNumber = 0;
             //floorNumber = 0;
         }
         else if(this.name.Equals("EndFloor"))
         {
             floorKind = (int)FloorKind.END_FLOOR;
-            trackNumber = 0;
+            //trackNumber = 0;
             floorNumber = -1;
         }
     }

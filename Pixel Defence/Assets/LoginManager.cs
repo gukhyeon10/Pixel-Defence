5dda9c340765885c5293206bd2e211976dea1e43
GUK <gukhyeon10@daum.net>
2019-09-30 01:55:40 +0900

    Login Scene 1.0.0
    로그인 씬 시작

---
+++ Pixel Defence/Assets/LoginManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginManager : MonoBehaviour
+{
+    [SerializeField]
+    UISprite sprite_BG;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void OnDestroy()
+    {
+    }
+    
+}
